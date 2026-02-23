b0e0bae556844cb6bc6f5fecbdf5d0801716af04
nafiuishaaq <nafiuig@gmail.com>
2026-02-23 14:36:06 +0100

    implemented the store

--- frontend/store/auth.store.ts
+++ frontend/store/auth.store.ts
@@ -40,6 +40,7 @@ export const useAuthStore = create<AuthStore>()(
       
       logout: () => {
         localStorage.removeItem('token');
+        localStorage.removeItem('user');
         document.cookie = 'auth-token=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT';
         set({
           token: null,
